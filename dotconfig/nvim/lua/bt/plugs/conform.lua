884adb68ec5ad9141d8d4a8cefc01afedca73dc2
timofey basuev <echiauta@gmail.com>
2025-08-23 12:50:48 +0500

    new nvim and fish

--- dotconfig/nvim/lua/bt/plugs/conform.lua
+++ dotconfig/nvim/lua/bt/plugs/conform.lua
@@ -1,24 +1,24 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				go = { "gofmt", "goimports" },
-				lua = { "stylua" },
-				proto = { "buf" },
-				json = { "prettier" },
-				-- markdown = { "prettier" },
-				yaml = { "prettier" },
-				sql = { "pg_format" },
-			},
-			format_after_save = {
-				lsp_format = "fallback",
-			},
-			-- format_on_save = {
-			-- 	lsp_format = "fallback",
-			-- 	timeout_ms = 500,
-			-- },
-		})
-	end,
+    "stevearc/conform.nvim",
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                go = { "gofumpt", "goimports" },
+                lua = { "stylua" },
+                proto = { "buf" },
+                json = { "prettier" },
+                -- markdown = { "prettier" },
+                -- yaml = { "prettier" },
+                sql = { "pg_format" },
+            },
+            format_after_save = {
+                lsp_format = "fallback",
+            },
+            -- format_on_save = {
+            -- 	lsp_format = "fallback",
+            -- 	timeout_ms = 500,
+            -- },
+        })
+    end,
 }
